5664b5c33604e046685a72acfe127c08ad82eea4
pro-coder520 <iremideadeyanju9@gmail.com>
2025-10-22 23:37:23 +0100

    Fixed some more errors

--- string_analyzer/settings.py
+++ string_analyzer/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = False
 
 
